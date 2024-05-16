89a2b887e30ee26dc7c0afd69add4a9cbeaa4587
Deng Changdong <dengchangdong@gmail.com>
2024-05-16 20:16:26 +0800

    Update hugo.py

--- hexoweb/libs/platforms/configs/hugo.py
+++ hexoweb/libs/platforms/configs/hugo.py
@@ -21,7 +21,7 @@ config = {
         "type": ["_index.md", "index.html", "index.md", ".md", ".html"],
         "save_path": "content/${filename}/index.md",
         "scaffold": "archetypes/page.md",
-        "excludes": ["post", "_drafts"]
+        "excludes": ["posts", "_drafts"]
     },
     "configs": {
         "path": [".github", "", "themes", "config"],
